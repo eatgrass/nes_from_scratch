27530bfa4c9e6c3d0ab97cb5e97e9f76f6deb319
Huang Jing <eatgrass@live.cn>
2023-08-23 19:22:15 +0800

    crate

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,9 @@
-pub struct CPU {
-    pub status: u8,
-    pub program_counter: u16,
-}
+pub mod cpu;
+pub mod instructions;
+mod nes;
+mod mem;
 
-impl CPU {
-    pub fn new() -> Self {
-        CPU {
-            // ┌─┬─┬──┬─┬─┬─┬─┐
-            // │N│V│ss│D│I│Z│C│
-            // └─┴─┴──┴─┴─┴─┴─┘
-            status: 0,
-            program_counter: 0,
-        }
-    }
-}
 
-impl Default for CPU {
-    fn default() -> Self {
-        Self::new()
-    }
-}
 
 fn main() {
     println!("Hello, world!");
